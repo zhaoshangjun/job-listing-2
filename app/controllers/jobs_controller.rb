2892e4e7bf34ec26a912f66746d609e748817b19
zhaoshangjun <zhaoshangjun@gmail.com>
2017-01-15 21:02:59 +0800

    implement job CRUD

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,63 @@
+class JobsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  def show
+    @job =Job.find(params[:id])
+
+  end
+
+  def index
+    flash[:notice] = "觉得这个招聘网站做得不错的话，就投我一票吧 : )"
+    @jobs=Job.all
+
+  end
+
+  def new
+    @job =Job.new
+
+  end
+
+  def created
+    @job=Job.new(job_params)
+
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+
+  def edit
+    @job = Job.find(params[:id])
+
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job =Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_path
+
+  end
+
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+
+  end
+
+
+
+
+end
